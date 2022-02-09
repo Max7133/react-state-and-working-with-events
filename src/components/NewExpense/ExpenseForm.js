d694c9dc9e10e3fcff10d614360a6206595ecf6a
Maksim Pasnitsenko <emexpas@outlook.com>
2022-02-09 13:42:29 +0200

    using one state instead of 3

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,23 +2,43 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState("");
+  /* const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
+  const [enteredDate, setEnteredDate] = useState(""); */
+  // I could pass in 1 useState instead of 3
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
   // Handling user input
   // JS way
   // document.getElementById('').addEventListener('click', (event) => {})
   // React way
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    // setEnteredTitle(event.target.value);
+    setUserInput({
+      // takes an object, pull out all the key value pairs,
+      // and adds them to this new object, and then I can still override key value pairs
+      ...userInput,
+      enteredTitle: event.target.value,
+    });
     console.log(enteredTitle);
   };
   const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
+    //setEnteredAmount(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value,
+    });
     console.log(enteredAmount);
   };
   const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
+    //setEnteredDate(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value,
+    });
     console.log(enteredDate);
   };
   return (
