f18c5d2158cff3c5f8c3d3c1f22d79e7b2502c47
Maksim Pasnitsenko <emexpas@outlook.com>
2022-03-15 14:56:08 +0200

    added 2 buttons for cancelling and adding expense

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "../NewExpense/ExpenseForm";
 import "./NewExpense.css";
 
 // in App.js <NewExpense onAddExpense={addExpenseHandler} />
 const NewExpense = (props) => {
+  // true or false state, to show or not show the form
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       // getting the object from ExpenseForm (line 58)
@@ -13,12 +16,31 @@ const NewExpense = (props) => {
     };
     // because of the name in App.js <NewExpense onAddExpense={addExpenseHandler} />
     props.onAddExpense(expenseData);
+    setIsEditing(false);
   };
+
+  // Should be triggered when Add New Expense button is clicked
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
   // 1st step, passing expenseData that I get from ExpenseForm component to the NewExpense component (line 18)
   // onSaveExpenseData(line 21) receivses saveExpenseDataHandler (line 7) as a value
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />;
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
